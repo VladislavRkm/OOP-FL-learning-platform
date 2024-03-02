1c863a5a903b14eff74e54d2e35556511dede03f
Maxim <markadok12@gmail.com>
2024-03-02 20:04:18 +0300

    OOPS-9, поменял типы полей

--- EngLeash/src/Application/EngLeash.Application.Models/Entities/LessonPassed.cs
+++ EngLeash/src/Application/EngLeash.Application.Models/Entities/LessonPassed.cs
@@ -1,7 +1,8 @@
 public class LessonPassed
 {
 	public int LessonPassedId { get; set; }
-	public int LessonId { get; set; }
-	public int UserId { get; set; }
+	public Lesson LessonId { get; set; }
+	public User UserId { get; set; }
 	public DateTime LessonPassedDate { get; set; }
+    public bool LessonIsPassed { get; set; }
 }
