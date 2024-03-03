c6f82e29d67907243a63ba4ce2e8117ff815fd17
Maxim <markadok12@gmail.com>
2024-03-03 23:55:32 +0300

    OOPS-10, поменял типы полей

--- EngLeash/src/Application/EngLeash.Application.Models/Entities/Lesson.cs
+++ EngLeash/src/Application/EngLeash.Application.Models/Entities/Lesson.cs
@@ -3,5 +3,5 @@ public class Lesson
     public int LessonId { get; set; }
     public string Title { get; set; }
     public string Content { get; set; }
-    public int CourseId { get; set; }
+    public Course CourseId { get; set; }
 }
