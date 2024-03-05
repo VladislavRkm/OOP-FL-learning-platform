b2e41b858cdcc26847c8a8e18375bd3424a90ef4
mkhivchk <370409@edu.itmo.ru>
2024-03-05 23:44:50 +0300

    Внес правки в соответствии с комментариями

--- EngLeash/src/Application/EngLeash.Application.Models/Interfaces/ILesson.cs
+++ EngLeash/src/Application/EngLeash.Application.Models/Interfaces/ILesson.cs
@@ -3,6 +3,5 @@ public interface ILesson
     public void CreateLesson() { }
     public void EditLesson() { }
     public void DeleteLesson() { }
-    public void SetCourseForLesson() { }
     public void GetLesson() { }
 }
--- EngLeash/src/Application/EngLeash.Application.Models/Interfaces/IUser.cs
+++ EngLeash/src/Application/EngLeash.Application.Models/Interfaces/IUser.cs
@@ -3,4 +3,5 @@ public interface IUser
     public void CreateUser() { }
     public void EditUser() { }
     public void DeleteUser() { }
+    public void GetUser() { }
 }
